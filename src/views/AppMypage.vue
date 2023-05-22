e8e563916ce87293b6008001f94baae4a835f7b6
김지은 <jxixeun@gmail.com>
2023-05-23 01:53:09 +0900

    :lipstick: [Feat] MyPage 화면 생성

---
+++ src/views/AppMypage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="container">
+    <the-nav-bar></the-nav-bar>
+    <my-page-container />
+  </div>
+</template>
+
+<script>
+import TheNavBar from "@/components/TheNavBar.vue";
+import MyPageContainer from "@/components/mypage/MyPageContainer.vue";
+export default {
+  name: "MyPage",
+  components: { TheNavBar, MyPageContainer },
+  data() {
+    return {
+      message: "",
+    };
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.container {
+  background-color: #feff86;
+  background-color: #b0daff;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
