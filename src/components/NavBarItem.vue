8741cb82f5b4a59d4f6aae755850127fd74b73e4
김지은 <jxixeun@gmail.com>
2023-05-22 10:19:31 +0900

    :lipstick: [Feat] dropdown 메뉴 추가

--- src/components/NavBarItem.vue
+++ src/components/NavBarItem.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="d-flex align-items-center">
-    <button type="button" class="btn">
-      <font-awesome-icon icon="fa-solid fa-magnifying-glass" size="xl" />
-    </button>
-    <button type="button" class="btn" @click="$router.push({ path: '/login' })">
-      <font-awesome-icon icon="fa-solid fa-user" style="color: #000000" size="xl" />
-    </button>
-    <button type="button" class="btn">
-      <font-awesome-icon icon="fa-solid fa-bell" size="xl" />
-    </button>
+    <div id="menu">
+      <ul>
+        <li>
+          <a href="#"><font-awesome-icon icon="fa-solid fa-magnifying-glass" size="xl" /></a>
+        </li>
+        <li>
+          <a href="/login"
+            ><font-awesome-icon icon="fa-solid fa-user" style="color: #000000" size="xl"
+          /></a>
+          <ul>
+            <li id="profile">닉네임님 안녕하세요!</li>
+            <li>My Page</li>
+            <li>Logout</li>
+          </ul>
+        </li>
+        <li>
+          <a href="#"><font-awesome-icon icon="fa-solid fa-bell" size="xl" /></a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -27,39 +38,90 @@ export default {
 </script>
 
 <style scoped>
+* {
+  font-family: "Pretendard";
+}
+a {
+  text-decoration: none;
+  color: black;
+}
 .align-items-center {
   align-items: center !important;
 }
 .d-flex {
   display: flex !important;
 }
-/* .btn {
-  background-color: transparent;
-  border: none;
-  height: 60px;
-  margin: 0px 8px;
-} */
 
-.btn {
-  display: inline-flex;
-  align-items: center;
-  justify-content: center;
+/* dropdown*/
+#menu ul {
   padding: 0;
-  height: calc(1.5em + 1.5rem + 2px);
-  width: calc(1.5em + 1.5rem + 2px);
-  border: 0;
-  margin-right: 0.5rem !important;
-  font-size: 1rem;
-  border-radius: 0.65rem;
-  text-align: center;
-  vertical-align: middle;
-  cursor: pointer;
-  font-weight: 500;
-  line-height: 1.2;
-  background-color: transparent;
+  list-style: none;
+  position: relative;
+  display: inline-block;
 }
 
-.btn:hover {
+#menu ul li {
+  float: left;
+  padding: 0;
+}
+
+#menu ul li:hover {
+  cursor: pointer;
   background-color: rgba(254, 255, 134, 0.5);
+  border-radius: 12px;
+}
+
+#menu ul li a {
+  display: block;
+  margin: 10px;
+  padding: 4px;
+  /* padding: 16px 20px; */
+  color: black;
+  text-decoration: none;
+}
+
+#menu ul ul {
+  display: none;
+  background-color: white;
+  padding: 0px;
+  position: absolute;
+  top: 100%;
+  margin-top: 2px;
+  border-radius: 12px;
+  width: 200px;
+}
+
+#menu ul li:hover > ul {
+  display: block;
+}
+
+#menu ul ul li {
+  float: none;
+  text-align: center;
+  position: relative;
+  padding: 10px;
+  font-size: 14px;
+}
+#menu ul ul li:hover {
+  border-radius: 0px;
+}
+
+#menu ul ul li:last-child:hover {
+  border-bottom-right-radius: 12px;
+  border-bottom-left-radius: 12px;
+}
+
+#menu ul ul ul {
+  position: absolute;
+  top: -1px;
+  left: 100%;
+  width: 120%;
+}
+#profile {
+  background-color: rgb(254, 255, 134);
+  border-top-right-radius: 12px;
+  border-top-left-radius: 12px;
+  pointer-events: none;
+  font-weight: 700;
 }
 </style>
