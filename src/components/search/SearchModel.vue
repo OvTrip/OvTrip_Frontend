23ca621a9d0e240eefc435f19b3fe199a9bb65ea
김지은 <jxixeun@gmail.com>
2023-05-24 18:01:08 +0900

    :zap: [Feat] 검색 모달창 생성

---
+++ src/components/search/SearchModel.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search-modal-container">검색모달입니당..</div>
+</template>
+
+<script>
+export default {
+  name: "SearchModal",
+  components: {},
+  data() {
+    return {
+      message: "",
+    };
+  },
+  created() {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.search-modal-container {
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  z-index: 100;
+  background-color: rgba(255, 255, 255, 0.5);
+  top: 0;
+  left: 0;
+  right: 0;
+}
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,8 @@ import Vue from "vue";
 import Vuex from "vuex";
 import createPersistedState from "vuex-persistedstate";
 import userStore from "@/store/modules/userStore";
+import modalStore from "@/store/modules/modalStore";
+
 
 Vue.use(Vuex);
 
@@ -10,7 +12,7 @@ export default new Vuex.Store({
   getters: {},
   mutations: {},
   actions: {},
-  modules: { userStore },
+  modules: { userStore, modalStore },
   plugins: [
     createPersistedState({
       // 브라우저 종료시 제거하기 위해 localStorage가 아닌 sessionStorage로 변경. (default: localStorage)
