4324ed707c15f0aa8146df14c93389624501b12a
김지은 <jxixeun@gmail.com>
2023-05-25 20:03:40 +0900

    :zap: [Feat] 마이페이지에서 여행 계획 누르면 여행 계획 페이지로 이동하도록 설정

--- src/components/mypage/MyPagePlanItem.vue
+++ src/components/mypage/MyPagePlanItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="#" class="col-sm-6 col-md-4" style="position: relative">
+  <a :href="`/plan/${plan.planId}`" class="col-sm-6 col-md-4" style="position: relative">
     <img
       src="https://images.unsplash.com/photo-1423589989400-cc0270157ed0?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=a95937ce1dc3243f52c30f86d948b771"
       alt=""
